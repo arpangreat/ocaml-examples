d97cadc9f9a32e771162967c649c66f68216a31d
arpangreat <arpanthegreat41@gmail.com>
2024-02-12 03:12:34 +0530

    clean ups

--- first/bin/main.ml
+++ first/bin/main.ml
@@ -2,28 +2,45 @@ let () =
   print_endline "Hello, World!";
 
   let x = 5 in
-  print_endline @@ string_of_int x;
+  print_endline @@ string_of_int x
+;;
 
-  let y = 4 |> succ in
-  print_endline @@ string_of_int y;
+let y = 4 |> succ in
+print_endline @@ string_of_int y
+;;
 
-  let z = match not true with true -> "true" | false -> "false" in
-  print_endline z;
+let z = match not true with true -> "true" | false -> "false" in
+print_endline z
 
-  (* let n = match 6 with x -> x | y -> y | _ -> 8 in
-     print_endline (string_of_int n); *)
-  let b = match [ "swastik"; "acharyya" ] with [] -> "coder" | h :: _t -> h in
-  print_endline b;
+let x = 5
+let y = 6
 
-  let fst3 t = match t with a, _, _ -> a in
+let n a =
+  match a with
+  | 5 -> print_endline @@ string_of_int x
+  | 6 -> print_endline @@ string_of_int y
+  | _ -> print_endline "not found"
+;;
 
-  let a = fst3 (1, 2, 3) in
-  print_endline @@ string_of_int a;
+n 4;;
 
-  let rec sum = function [] -> 0 | h :: t -> h + sum t in
-  let a = sum [ 1; 2; 3; 4; 5 ] in
-  print_endline @@ string_of_int a;
+let b = match [ "swastik"; "acharyya" ] with [] -> "coder" | h :: _t -> h in
+print_endline b
 
-  let print1 x = print_endline x in
+let fst3 t = match t with a, _, _ -> a;;
 
-  print1 "string"
+let a = fst3 (1, 2, 3) in
+print_endline @@ string_of_int a
+
+let rec sum = function [] -> 0 | h :: t -> h + sum t;;
+
+let a = sum [ 1; 2; 3; 4; 5 ] in
+print_endline @@ string_of_int a
+
+let print1 x = print_endline x;;
+
+print1 "string"
+
+let avg a b = (a +. b) /. 2.0;;
+
+print_endline @@ string_of_float (avg 5.0 6.0)
