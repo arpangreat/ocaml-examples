27933e4c468f999f93278df0192e8138fdb38a82
arpangreat <arpanthegreat41@gmail.com>
2024-02-18 18:59:11 +0530

    fold, map and higher order functions

--- first/bin/main.ml
+++ first/bin/main.ml
@@ -54,3 +54,51 @@ let rec list_max (lst : 'a list) : 'a option =
 let x = list_max [ 1; 2; 3; 4; 5 ];;
 
 print_endline @@ string_of_int (get_val 0 x)
+
+let double x = x * 2
+let twice f x = f (f x)
+let quad x = twice double x;;
+
+print_endline @@ string_of_int (quad 5)
+
+type 'a tree = Leaf | Node of 'a * 'a tree * 'a tree
+
+let t = Node (1, Node (2, Leaf, Leaf), Node (3, Leaf, Leaf))
+let rec size = function Leaf -> 0 | Node (_, l, r) -> 1 + size l + size r
+let rec sum = function Leaf -> 0 | Node (x, l, r) -> x + sum l + sum r;;
+
+print_endline @@ string_of_int (size t) ^ " size of tree";;
+print_endline @@ string_of_int (sum t) ^ " sum of tree"
+
+(* let rec add = function [] -> [] | h :: t -> (h + 1) :: add t *)
+let rec map f = function [] -> [] | h :: t -> f h :: map f t
+let add' = map (fun x -> x + 1)
+
+let rec combine init op = function
+  | [] -> init
+  | h :: t -> op h (combine init op t)
+;;
+
+let add'' lst = combine 0 ( + ) lst
+
+let rec fold_right f acc = function
+  | [] -> acc
+  | h :: t -> f h (fold_right f t acc)
+;;
+
+let rec fold_left f acc = function
+  | [] -> acc
+  | h :: t ->
+      let acc' = f acc h in
+      fold_left f acc' t
+;;
+
+let rec print_lst = function
+  | [] -> ()
+  | h :: t ->
+      print_endline @@ string_of_int h;
+      print_lst t
+;;
+
+print_lst (add' [ 1; 2; 3; 4; 5 ])
+(* print_lst (add'' [ 1; 2; 3; 4; 5 ]) *)
